c27bc4dcd3804456118bd6f3878185ac0d579958
ドイツ <thd1152015@gmail.com>
2024-06-19 22:38:36 +0700

    Update Theme

--- services/my-blogs-ui/src/routes/+layout.svelte
+++ services/my-blogs-ui/src/routes/+layout.svelte
@@ -1,12 +1,23 @@
 <script>
 	import '../app.css';
+	import NavBar from '$lib/components/NavBar.svelte';
+	let darkMode = false;
+
+	function toggleDarkMode() {
+		darkMode = !darkMode;
+		if (darkMode) {
+			document.documentElement.classList.add('dark');
+		} else {
+			document.documentElement.classList.remove('dark');
+		}
+	}
 </script>
 
-<nav>
-	<a href="/">Home</a>
-	<a href="/blog">Blog</a>
-	<a href="/about">About</a>
-</nav>
+<button on:click={toggleDarkMode} class="rounded bg-primary p-2 text-secondary">
+	Toggle Dark Mode
+</button>
+
+<NavBar></NavBar>
 
 <slot></slot>
 
--- services/my-blogs-ui/tailwind.config.ts
+++ services/my-blogs-ui/tailwind.config.ts
@@ -1,11 +1,60 @@
 import type { Config } from 'tailwindcss';
 
 export default {
+	darkMode: 'class',
 	content: ['./src/**/*.{html,js,svelte,ts}'],
-
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				primary: {
+					light: '#333333',
+					DEFAULT: '#000000',
+					dark: '#000000'
+				},
+				secondary: {
+					light: '#FFFFFF',
+					DEFAULT: '#FFFFFF',
+					dark: '#CCCCCC'
+				},
+				tertiary: {
+					light: '#E0E0E0',
+					DEFAULT: '#B0B0B0',
+					dark: '#808080'
+				},
+				success: {
+					light: '#A5D6A7',
+					DEFAULT: '#4CAF50',
+					dark: '#388E3C'
+				},
+				warning: {
+					light: '#FFE082',
+					DEFAULT: '#FFC107',
+					dark: '#FFA000'
+				},
+				error: {
+					light: '#EF9A9A',
+					DEFAULT: '#F44336',
+					dark: '#D32F2F'
+				},
+				surface: {
+					light: '#F5F5F5',
+					DEFAULT: '#EEEEEE',
+					dark: '#E0E0E0'
+				},
+				text: {
+					light: '#FFFFFF',
+					DEFAULT: '#000000',
+					dark: '#000000'
+				},
+				background: {
+					light: '#FFFFFF',
+					DEFAULT: '#F5F5F5',
+					dark: '#121212' // Dark mode background color
+				}
+			}
+		}
 	},
 
+	// eslint-disable-next-line @typescript-eslint/no-require-imports
 	plugins: [require('@tailwindcss/typography')]
 } as Config;
