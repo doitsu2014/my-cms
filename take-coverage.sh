3eca73b5b684fd83cb620de2460e654c90ee0d11
ドイツ <thd1152015@gmail.com>
2025-04-01 09:58:22 +0700

    Update CI configuration and scripts for improved coverage reporting and toolchain management

--- take-coverage.sh
+++ take-coverage.sh
@@ -1,10 +1,10 @@
-rustup toolchain add nightly --component llvm-tools-preview
-rustup override set nightly
+rustup toolchain add stable --component llvm-tools-preview
+rustup override set stable
 export RUSTC_BOOTSTRAP=1
-export RUSTFLAGS='-Cinstrument-coverage'
+export RUSTFLAGS='-Cinstrument-coverage -Ccodegen-units=1 -Cllvm-args=--inline-threshold=0 -Clink-dead-code -Coverflow-checks=off -Cpanic=abort -Zpanic_abort_tests'
 export LLVM_PROFILE_FILE=my-cms-%p-%m.profraw
 cargo clean
-cargo test
+cargo test --all --all-features --no-fail-fast
 # LCOV
 # grcov . --binary-path ./target/debug/ -s . -t lcov --branch --ignore-not-existing --ignore "/*" -o lcov.info
 # HTML
