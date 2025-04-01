1c460348c3d87e3316a33c64543feea27ec998e7
ドイツ <thd1152015@gmail.com>
2025-04-01 10:52:00 +0700

    Update CI configuration and coverage scripts for improved reporting and cleanup

--- take-coverage.sh
+++ take-coverage.sh
@@ -1,12 +1,16 @@
-rustup toolchain add stable --component llvm-tools
+rustup toolchain add stable --component llvm-tools-preview
 rustup override set stable
 export RUSTC_BOOTSTRAP=1
 # export RUSTFLAGS='-Cinstrument-coverage -Ccodegen-units=1 -Cllvm-args=--inline-threshold=0 -Clink-dead-code -Coverflow-checks=off -Cpanic=abort -Zpanic_abort_tests'
 export RUSTFLAGS='-Cinstrument-coverage -Zpanic_abort_tests'
 export LLVM_PROFILE_FILE=test-llvm-reports/my-cms-%p-%m.profraw
 export CARGO_INCREMENTAL=0
+rm -rf **/test-llvm-reports
+rm -rf test-llvm-reports
+rm lcov.info
+cargo clean
 cargo test --all --all-features --no-fail-fast
 # LCOV
-grcov . --binary-path ./target/debug/ -s . -t lcov --branch --ignore-not-existing --ignore "/*" -o lcov.info
+grcov . --binary-path target/debug/deps/ -s . -t lcov --branch --ignore-not-existing --ignore "/*" -o lcov.info
 # HTML
 # grcov . -s . --binary-path ./target/debug/ -t html --branch --ignore-not-existing -o ./target/debug/coverage/
