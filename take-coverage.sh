416e8435408d8a6367a5ea80affad246b17eff9f
ドイツ <thd1152015@gmail.com>
2025-04-01 10:32:53 +0700

    Update coverage configuration and scripts for improved reporting and toolchain management

--- take-coverage.sh
+++ take-coverage.sh
@@ -1,11 +1,11 @@
-rustup toolchain add stable --component llvm-tools-preview
+rustup toolchain add stable --component llvm-tools
 rustup override set stable
 export RUSTC_BOOTSTRAP=1
-export RUSTFLAGS='-Cinstrument-coverage -Ccodegen-units=1 -Cllvm-args=--inline-threshold=0 -Clink-dead-code -Coverflow-checks=off -Cpanic=abort -Zpanic_abort_tests'
-export LLVM_PROFILE_FILE=my-cms-%p-%m.profraw
-cargo clean
+# export RUSTFLAGS='-Cinstrument-coverage -Ccodegen-units=1 -Cllvm-args=--inline-threshold=0 -Clink-dead-code -Coverflow-checks=off -Cpanic=abort -Zpanic_abort_tests'
+export RUSTFLAGS='-Cinstrument-coverage -Zpanic_abort_tests'
+export LLVM_PROFILE_FILE=profile/my-cms-%p-%m.profraw
 cargo test --all --all-features --no-fail-fast
 # LCOV
-# grcov . --binary-path ./target/debug/ -s . -t lcov --branch --ignore-not-existing --ignore "/*" -o lcov.info
+grcov . --binary-path ./target/debug/ -s . -t lcov --branch --ignore-not-existing --ignore "/*" -o lcov.info
 # HTML
-grcov . -s . --binary-path ./target/debug/ -t html --branch --ignore-not-existing -o ./target/debug/coverage/
+# grcov . -s . --binary-path ./target/debug/ -t html --branch --ignore-not-existing -o ./target/debug/coverage/
