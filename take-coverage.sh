581edd902e4fdd1cc5a7abdc250beeaccc9fb126
ドイツ <thd1152015@gmail.com>
2025-04-01 10:43:04 +0700

    Update CI and coverage configurations for improved reporting and ignore patterns

--- take-coverage.sh
+++ take-coverage.sh
@@ -3,7 +3,8 @@ rustup override set stable
 export RUSTC_BOOTSTRAP=1
 # export RUSTFLAGS='-Cinstrument-coverage -Ccodegen-units=1 -Cllvm-args=--inline-threshold=0 -Clink-dead-code -Coverflow-checks=off -Cpanic=abort -Zpanic_abort_tests'
 export RUSTFLAGS='-Cinstrument-coverage -Zpanic_abort_tests'
-export LLVM_PROFILE_FILE=profile/my-cms-%p-%m.profraw
+export LLVM_PROFILE_FILE=test-llvm-reports/my-cms-%p-%m.profraw
+export CARGO_INCREMENTAL=0
 cargo test --all --all-features --no-fail-fast
 # LCOV
 grcov . --binary-path ./target/debug/ -s . -t lcov --branch --ignore-not-existing --ignore "/*" -o lcov.info
